03c771fa2977741a0429e34f5e70685b119fb99c
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-02-18 00:53:24 +0530

    added configuration class

--- Back-end/src/main/java/lk/spring/easyCar/WebAppInitializer.java
+++ Back-end/src/main/java/lk/spring/easyCar/WebAppInitializer.java
@@ -1,20 +1,22 @@
 package lk.spring.easyCar;
 
+import lk.spring.easyCar.config.WebAppConfig;
+import lk.spring.easyCar.config.WebRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
---
+++ Back-end/src/main/java/lk/spring/easyCar/config/WebAppConfig.java
@@ -0,0 +1,11 @@
+package lk.spring.easyCar.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebAppConfig {
+}
