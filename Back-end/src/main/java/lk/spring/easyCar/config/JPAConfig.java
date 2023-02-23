bf1c93d0945130108776476ca09f08a3a34f0487
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-02-23 16:10:33 +0530

    added dependency & jpa configurationa

--- Back-end/src/main/java/lk/spring/easyCar/config/JPAConfig.java
+++ Back-end/src/main/java/lk/spring/easyCar/config/JPAConfig.java
@@ -1,5 +1,49 @@
 package lk.spring.easyCar.config;
 
-public class JPAConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+public class JPAConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
+        LocalContainerEntityManagerFactoryBean bean= new LocalContainerEntityManagerFactoryBean();
+        bean.setPackagesToScan("lk.spring.easyCar.entity");
+        bean.setDataSource(ds);
+        bean.setJpaVendorAdapter(jpa);
+        return bean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/easyCar?createDatabaseIfNotExist=true");
+        ds.setUsername("root");
+        ds.setPassword("1234");
+        return ds;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter va=new HibernateJpaVendorAdapter();
+        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        va.setDatabase(Database.MYSQL);
+        va.setGenerateDdl(true);
+        va.setShowSql(true);
+        return va;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
 }
--- Back-end/src/main/java/lk/spring/easyCar/config/WebRootConfig.java
+++ Back-end/src/main/java/lk/spring/easyCar/config/WebRootConfig.java
@@ -1,7 +1,17 @@
 package lk.spring.easyCar.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({JPAConfig.class})
+@ComponentScan(basePackages = "lk.spring.easyCar.service.impl")
 public class WebRootConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
