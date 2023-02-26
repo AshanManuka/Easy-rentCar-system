7241b7b158da7eb0036ec4462fa816c7ba727778
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-02-26 17:55:45 +0530

    done accept requests by admin

--- Front-end/event/js/manageCustomer.js
+++ Front-end/event/js/manageCustomer.js
@@ -1,7 +1,7 @@
 // ===================Manage Customer==============
 
 let baseURL = "http://localhost:8080/app/";
-
+let requestList = [];
 
  // ========================= load all Customer =========================
 
@@ -19,10 +19,24 @@ function loadPendingCustomer(){
         dataType: "json",
         success: function (resp) {
             for (let c of resp.data) {
-                let name = c.userName;
-                let email = c.email;
-                let contact = c.contact;
+                let idd = c.id;
+                let na = c.userName;
+                let pas = c.password;
+                let em = c.email;
+                let co = c.contact;
                 let image = c.nicImage;
+
+                var customer = {
+                    id : idd,
+                    userName : na,
+                    password : pas,
+                    email : em,
+                    contact: co,
+                    incImage : image
+                }
+
+                requestList.push(customer);
+                console.log(requestList);
 
                 if(image !== null){
                    imgStatus = "NIC Verified";
@@ -30,7 +44,7 @@ function loadPendingCustomer(){
                    imgStatus = "NIC Not Verified";
                 }
 
-                 var e = $(`<br><div style="display:block; background-color: rgba(10,87,102,0.65); border-radius:20px; float:left; width:`+800+`px;   margin-left:`+50+`px;border:2px solid #cccccc;"><h4 style="margin-left: 30px; color: azure; display: inline; width: auto" >${name}</h4><h4 style="color: azure; margin-left: 20px; display: inline">${email}</h4><h4 style="color: azure; margin-left: 20px; display: inline">${contact}</h4><h4 style="color: azure; margin-left: 20px; display: inline">${imgStatus}</h4> <button style="background-color: #a52834; border-radius: 40px; width: 100px; height: 40px; display: inline; margin-left: 20px" class="btnClass" id="#`+ids+`"  data-bs-toggle="modal"  type="button"><b>Accept</b></button><button style="background-color: #a52834; border-radius: 40px; width: 100px; height: 40px; margin-top: 20px; margin-bottom:20px; margin-left: 20px " class="buyTicket" id="btnTwo" data-bs-toggle="modal"  type="button"><b>Deny</b></button></div><br>`);
+                 var e = $(`<br><div style="display:block; background-color: rgba(10,87,102,0.65); border-radius:20px; float:left; width:`+800+`px;   margin-left:`+50+`px;border:2px solid #cccccc;"><h4 style="margin-left: 30px; color: azure; display: inline; width: auto" >${na}</h4><h4 style="color: azure; margin-left: 20px; display: inline">${em}</h4><h4 style="color: azure; margin-left: 20px; display: inline">${co}</h4><h4 style="color: azure; margin-left: 20px; display: inline">${imgStatus}</h4> <button style="background-color: #a52834; border-radius: 40px; width: 100px; height: 40px; display: inline; margin-left: 20px" class="btnClass" id="#`+ids+`"  data-bs-toggle="modal"  type="button"><b>Accept</b></button><button style="background-color: #a52834; border-radius: 40px; width: 100px; height: 40px; margin-top: 20px; margin-bottom:20px; margin-left: 20px " class="buyTicket" id="btnTwo" data-bs-toggle="modal"  type="button"><b>Deny</b></button></div><br>`);
                  e.attr('id', 'myid');
                  $('#reqList').append(e);
                  ids++;
@@ -41,6 +55,34 @@ function loadPendingCustomer(){
 
 }
 
+$("#acceptBtn").click(function () {
+
+
+    for (let i = 0; i<requestList.length; i++){
+        var Customer = requestList[i];
+
+        $.ajax({
+            url: baseURL+'customer',
+            method: 'post',
+            contentType:"application/json",
+            data:JSON.stringify(Customer),
+            dataType:"json",
+            success: function (res) {
+                alert(res.message);
+            },
+            error:function (error){
+                let cause= JSON.parse(error.responseText).message;
+                alert(cause);
+            }
+
+        });
+    }
+
+
+
+
+
+});
 
 
 
