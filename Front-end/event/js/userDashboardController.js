67d111d30c0f6112d8785454a5e95c5ed4081677
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-03-04 12:48:53 +0530

    done load car details to sweet alert

--- Front-end/event/js/userDashboardController.js
+++ Front-end/event/js/userDashboardController.js
@@ -8,23 +8,14 @@ loadAllCars();
 
 
 $("#searchBtn").click(function(){
-    Swal.fire({
-        title: '<img src="../../event/images/carLogo.jpg" width="200px" height="200px"> <img src="../../event/images/carLogo.jpg" width="200px" height="200px"> <img src="../../event/images/carLogo.jpg" width="200px" height="200px"> <img src="../../event/images/carLogo.jpg" width="200px" height="200px">',
-        html:
-            'Transition : <b>Auto</b> <br> Fuel : <b>Petrol</b> <br> Daily : <b>10000</b> <br> Monthly : <b>100000</b> <br> Extra KM : <b>800</b>',
 
-        showCloseButton: true,
-        focusConfirm: false,
-        confirmButtonText:
-            '<i class="fa-sharp fa-solid fa-cart-shopping"></i> BUY',
-        confirmButtonAriaLabel: 'Thumbs up, great!',
-
-    })
 });
 
 
 
 // ============================ load all Cars on feed ===============================
+
+        var idCount = 1;
 
         function loadAllCars(){
             $.ajax({
@@ -47,18 +38,43 @@ $("#searchBtn").click(function(){
 
 
 
-                        var ii = "../../event/images/cars/alto2.jpg"
-
-
                         var perPart = "../../event/images/cars/";
-                        var lastPart = iOne.substring(12,21);
-                        var fullPart = perPart+lastPart;
-                        console.log(fullPart);
+                        var lastPartOne = iOne.substring(12,21);
+                        var lastPartTwo = iTwo.substring(12,21);
+                        var lastPartThree = iThree.substring(12,21);
+                        var lastPartFour = iFour.substring(12,21);
+                        var lastPartFive = iFive.substring(12,21);
+                        var fullPartOne = perPart+lastPartOne;
+                        var fullPartTwo = perPart+lastPartTwo;
+                        var fullPartThree = perPart+lastPartThree;
+                        var fullPartFour = perPart+lastPartFour;
+                        var fullPartFive = perPart+lastPartFive;
+                        console.log("id Count"+idCount);
 
-                            var e = $(`<div style="display:block; background-color: rgba(16,0,3,0.25); border-radius:5px; float:left;width:`+300+`px; height:`+320+`px; margin-top:`+50+`px;margin-left:`+50+`px;border:1px solid #CCCCCC;"><img src=`+fullPart+` alt="image" style="width:`+200+`px; height:`+230+`px; margin-left:`+50+`px; margin-top:`+20+`px; border-radius: 5px"><br><br><button style="border-radius: 20px; border: black solid 1px; margin-left: 70px; width: 150px; height: 30px; background-color: #87d0de"><b>More</b></button> </div>`);
-                            e.attr('id', 'carId');
+                            var e = $(`<div style="display:block; background-color: rgba(16,0,3,0.25); border-radius:5px; float:left;width:`+300+`px; height:`+320+`px; margin-top:`+50+`px;margin-left:`+50+`px;border:1px solid #CCCCCC;"><img src=`+fullPartOne+` alt="image" style="width:`+270+`px; height:`+230+`px; margin-left:`+13+`px; margin-top:`+20+`px; border-radius: 5px"><br><br><button style="border-radius: 20px; border: black solid 1px; margin-left: 70px; width: 150px; height: 30px; background-color: #87d0de"><b>More</b></button> </div>`);
+                            e.attr('class', 'idCount');
                             $('#vehicleFeed').append(e);
+                            idCount++;
 
+
+
+
+                        $(".idCount").click(function(){
+
+                            Swal.fire({
+                                title: '<img src='+fullPartTwo+' width="200px" height="200px"> <img src='+fullPartThree+' width="200px" height="200px"> <img src='+fullPartFour+' width="200px" height="200px"> <img src='+fullPartFive+' width="200px" height="200px">',
+                                html:
+                                    'Transition : <b>'+tr+'</b> <br> Fuel : <b>'+fType+'</b> <br> Daily : <b>'+dRate+'</b> <br> Monthly : <b>'+mRate+'</b> <br> Extra KM : <b>'+exKM+'</b>',
+
+                                showCloseButton: true,
+                                focusConfirm: false,
+                                confirmButtonText:
+                                    '<i class="fa-sharp fa-solid fa-cart-shopping"></i> BUY',
+                                confirmButtonAriaLabel: 'Thumbs up, great!',
+
+                            })
+
+                        });
 
 
                         // var car = {
@@ -80,6 +96,8 @@ $("#searchBtn").click(function(){
                         // }
 
                     }
+
+
                 }
             });
 
