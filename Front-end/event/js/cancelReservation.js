56d83326560f00e8c55985d3d820a9926dcdefe8
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-03-08 17:22:27 +0530

    done show message function on userdashboard

--- Front-end/event/js/cancelReservation.js
+++ Front-end/event/js/cancelReservation.js
@@ -1,32 +1,68 @@
 
     let baseURL = "http://localhost:8080/app/";
 
-loadAllMessages();
 
 
 // ========= load messages ==================
-//when clicking btn load data from two tablles
+
+    $("#findBtn").click(function(){
+        let mail = $("#mailAddress").val();
+        loadAllMessages(mail);
+        loadAllRes(mail);
+    });
 
 
-function loadAllMessages(){
+
+
+function loadAllMessages(mail){
     $.ajax({
         url: baseURL+'cancelRequest',
         method: 'get',
         dataType: "json",
         success: function (resp) {
             for (let c of resp.data) {
-                let regNo = c.vehiNo;
-                let rDate = "2023-00-11";
-                let rStatus = "Not Approved";
-                let rMessage = c.message;
+                if (mail == c.userMail){
+                    let regNo = c.vehiNo;
+                    let rDate = "2023-00-11";
+                    let rStatus = "Not Approved";
+                    let rMessage = c.message;
+
+                    var row = "<tr class='nr'><td style='background-color: rgba(250,11,34,0.25)'>" + regNo + "</td><td style='background-color: rgba(8,45,229,0.25)'>" + rDate + "</td><td style='background-color: rgba(95,250,11,0.25)'>" + rStatus + "</td><td style='background-color: rgba(250,182,11,0.25)'>" + rMessage + "</td></tr>";
+                    $("#messageTable").append(row);
+                }
 
 
 
-
-                var row = "<tr class='nr'><td style='background-color: rgba(250,11,34,0.25)'>" + regNo + "</td><td style='background-color: rgba(8,45,229,0.25)'>" + rDate + "</td><td style='background-color: rgba(95,250,11,0.25)'>" + rStatus + "</td><td style='background-color: rgba(250,182,11,0.25)'>" + rMessage + "</td></tr>";
-                $("#messageTable").append(row);
             }
         }
 
     });
+}
+
+
+function loadAllRes(mail){
+
+    $.ajax({
+        url: baseURL+'reservation',
+        method: 'get',
+        dataType: "json",
+        success: function (resp) {
+            for (let c of resp.data) {
+                if (mail == c.userMail){
+                    let regNo = c.carNo;
+                    let rDate = c.pickupDate;
+                    let rStatus = "Approved";
+                    let rMessage = "We Will Contact You...";
+
+                    var row = "<tr class='nr'><td style='background-color: rgba(250,11,34,0.25)'>" + regNo + "</td><td style='background-color: rgba(8,45,229,0.25)'>" + rDate + "</td><td style='background-color: rgba(95,250,11,0.25)'>" + rStatus + "</td><td style='background-color: rgba(250,182,11,0.25)'>" + rMessage + "</td></tr>";
+                    $("#messageTable").append(row);
+                }
+
+
+
+            }
+        }
+
+    });
+
 }
